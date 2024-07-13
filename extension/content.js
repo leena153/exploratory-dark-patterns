bfcdbda03c2812b4a4b53d4dfde0a5eb1b2c31cd
Leena Gupta <leenaguptalg2@gmail.com>
2024-07-14 01:45:45 +0530

    child link logic

--- extension/content.js
+++ extension/content.js
@@ -43,7 +43,6 @@ function recurse(i) {
         var cleanedText = i.innerText.trim().replace("\n", "");
         cleanedText = rmPriceTags(cleanedText);
         if (cleanedText.length >= 6) {
-          gfg.push(i);
           text.push(cleanedText);
         }
       }
@@ -61,27 +60,90 @@ function recurse(i) {
   }
 }
 
+function recurse_childLinks(i) {
+  if (
+    !(
+      i.nodeType === 8 ||
+      i.tagName === "SCRIPT" ||
+      i.tagName === "STYLE" ||
+      i.tagName === "NOSCRIPT"
+    )
+  ) {
+    if (i.childElementCount == 0) {
+      if (i.innerText != "" && i.innerText != null && isNodeVisible(i)) {
+        var cleanedText = i.innerText.trim().replace("\n", "");
+        cleanedText = rmPriceTags(cleanedText);
+        if (cleanedText.length >= 6) {
+          gfg.push(i);
+          text.push(cleanedText);
+        }
+      }
+    } else {
+      for (j of i.childNodes) {
+        recurse_childLinks(j);
+      }
+    }
+  }
+}
+
 console.log("content script");
 console.log("child links", child_links);
 recurse(document.body);
 
 var data = JSON.stringify(text);
 
-chrome.runtime.sendMessage({ message: "saveData", data: text });
+parent_url = window.location.href;
+var temp = {parent_url: parent_url, child_link: "/", content: data};
+data_dict = JSON.stringify(temp);
+
+chrome.runtime.sendMessage({ message: "saveData", data: data_dict});
 
 for (var k = 0; k < child_links.length; k++) {
   console.log("Scraping content from", child_links[k]);
   var childLink = child_links[k];
   fetch(childLink)
+    // .then((response) => response.text())
+    // .then((html) => {
+    // console.log(html)
+    // var parser = new DOMParser();
+    // var doc = parser.parseFromString(html, "text/html");
+    // var fetchedBody = doc.body;
     .then((response) => response.text())
     .then((html) => {
       var tempElement = document.createElement("div");
       tempElement.innerHTML = html;
       var scrapedContent = tempElement.innerText.trim().replace("\n", "");
-      scrapedContent = rmPriceTags(scrapedContent);
-      console.log("Scraped content from", childLink, ":", scrapedContent);
+      text.length = 0;
+      recurse_childLinks(scrapedContent);
+      console.log(text);
+      var data = JSON.stringify(text);
+      var temp = {
+        parent_url: parent_url,
+        child_link: childLink,
+        content: data,
+      };
+      data_dict = JSON.stringify(temp);
+
+      chrome.runtime.sendMessage({ message: "saveData", data: data_dict });
     })
     .catch((error) => {
       console.error("Error scraping content from", childLink, ":", error);
     });
 }
+
+// for (var k = 0; k < child_links.length; k++) {
+//   console.log("Scraping content from", child_links[k]);
+//   var childLink = child_links[k];
+//   fetch(childLink)
+//     .then((response) => response.text())
+//     .then((html) => {
+//       var tempElement = document.createElement("div");
+//       tempElement.innerHTML = html;
+//       var scrapedContent = tempElement.innerText.trim().replace("\n", "");
+//       scrapedContent = rmPriceTags(scrapedContent);
+//       console.log("Scraped content from", childLink, ":", scrapedContent);
+//     })
+//     .catch((error) => {
+//       console.error("Error scraping content from", childLink, ":", error);
+//     });
+// }
